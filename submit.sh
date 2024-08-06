89b0b44ed17201b25a01f682b9a5da2e52de8f14
James <1026@newbitcoincity.com>
2024-08-06 10:58:44 +0000

    fix wrong api call

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 # Define the URL with the query parameter
-URL="https://api-dojo2.eternalai.org/api/contest/submit-tx?tx_hash=$2?problem_id=$1"
+URL="https://api-dojo2.eternalai.org/api/contest/submit-tx?problem_id=$1&tx_hash=$2"
 
 # Make the GET request using curl
 RESPONSE=$(curl -s "$URL")
